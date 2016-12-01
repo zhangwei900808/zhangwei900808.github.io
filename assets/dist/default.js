953ea6d4b08e7cc4444210280d0063cee2fa13ca
awbeci <zw900808@gmail.com>
2016-12-01 21:34:43 +0800

    update

--- assets/dist/default.js
+++ assets/dist/default.js
@@ -3,4 +3,15 @@ $(function(){
     $('.categories').addClass('show-out');
     $('.home').addClass('show-out');
   })
+
+  $('.home').click(function(e){
+    $('.categories').removeClass('show-out');
+    $('.home').removeClass('show-out');
+  })
+
+  $('.left-container').click(function(e){
+    $('.categories').removeClass('show-out');
+    $('.home').removeClass('show-out');
+  })
+
 })
